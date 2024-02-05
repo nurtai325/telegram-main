ebe25f1f89d830c676fac5fcfc73c8b56044c2b2
nurtai325 <140373832+nurtai325@users.noreply.github.com>
2024-02-05 20:58:24 +0600

    .

--- app/lib/store/store.js
+++ app/lib/store/store.js
@@ -1,6 +1,9 @@
-import { create } from 'zustand'
+import { create } from 'zustand';
+import { chatLinkProps } from '../chatData';
 
 export const useStore = create((set) => ({
   logged: true,
   logIn: () => set((state) => ({ logged: true })),
+  filteredList: chatLinkProps,
+  setFilteredList: (list) => set((state) => ({ filteredList: list})),
 }))
--- app/ui/Searchbar.js
+++ app/ui/Searchbar.js
@@ -2,11 +2,14 @@
 import { SlMagnifier } from "react-icons/sl";
 import { useState } from "react";
 import { useEffect } from "react";
+import { useStore } from "../lib/store/store";
+import { chatLinkProps } from "../lib/chatData";
 
 export default function Searchbar(props) {
     const [value, setValue] = useState('');
     const [focus, setFocus] = useState(false);
     const [use, setUse] = useState(false);
+    const setFilteredList = useStore((state) => state.setFilteredList);
 
         useEffect(() => {
           if (!use) {
@@ -15,14 +18,22 @@ export default function Searchbar(props) {
             setFocus(true);
           }
         }, [use]);
-      
-    const handle = () => {
+      useEffect(() => {
+        if(value.length === 0) {
+          setFilteredList(chatLinkProps);
+        } else {
+          const list = chatLinkProps.filter(item => item.chat.toLowerCase().includes(value.toLowerCase()));
+          setFilteredList(list);
+        };
+      }, [value]);
+    const handle = (event) => {
+        setValue(event.target.value);
         
-    }
+    };
     return (
         <div className={`group w-[334px] h-[42px] mt-[0px] flex bg-black rounded-3xl border ${focus ? 'border-focus' : "border-stone-800"} border-[1px] ${focus ? '' : "hover:border-white"} `} >
             <SlMagnifier className="mt-3 ml-5 group-focus:fill-blue" size={17} color={focus ? '#8774e1' : '#aaaaaa'}/>
-            <input className={`border-none ml-4 focus:outline-none bg-transparent w-[248px] h-[38px] text-white`} placeholder="search" value={value} onChange={(e) => setValue(e.target.value)} onFocus={() => setUse(true)} onBlur={() => setUse(false)}/>
+            <input className={`border-none ml-4 focus:outline-none bg-transparent w-[248px] h-[38px] text-white`} placeholder="search" value={value} onChange={handle} onFocus={() => setUse(true)} onBlur={() => setUse(false)}/>
         </div>
     );
 };
