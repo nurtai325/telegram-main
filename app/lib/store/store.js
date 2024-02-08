eec884df9c9f6f248fe089235386a7aeca176930
nurtai325 <140373832+nurtai325@users.noreply.github.com>
2024-02-08 22:41:23 +0600

    .

--- app/lib/store/store.js
+++ app/lib/store/store.js
@@ -2,7 +2,7 @@ import { create } from 'zustand';
 import { chatLinkProps } from '../chatData';
 
 export const useStore = create((set) => ({
-  logged: false,
+  logged: true,
   logIn: () => set((state) => ({ logged: true })),
   filteredList: chatLinkProps,
   setFilteredList: (list) => set((state) => ({ filteredList: list})),
