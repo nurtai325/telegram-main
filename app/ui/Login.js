96eecfef5df0ae455d3d5482af0024ea18996bb7
nurtai325 <140373832+nurtai325@users.noreply.github.com>
2024-02-18 23:02:14 +0600

    added

--- app/ui/Login.js
+++ app/ui/Login.js
@@ -4,7 +4,7 @@ import { useState } from 'react';
 import Link from 'next/link';
 import axios from 'axios';
 import { useStore } from '../lib/store/store';
-
+//here 
 const Login = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
